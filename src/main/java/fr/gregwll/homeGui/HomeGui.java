f603e68d30421b09a105b13fd5776da768666e88
Grégoire Wild <115501088+NothingSpeci4l@users.noreply.github.com>
2026-05-02 19:07:24 +0200

    Initial Commit

---
+++ src/main/java/fr/gregwll/homeGui/HomeGui.java
@@ -0,0 +1,17 @@
+package fr.gregwll.homeGui;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class HomeGui extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
